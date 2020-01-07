a1599ef1adec702e49583a1ec8c58cf0ed45ac7f
Jeffrey Zhang <zhang.lei.fly@gmail.com>
2020-01-07 12:09:52 +0800

    use full parameter

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 docker run \
     --net host \
     --privileged \
-    -it \
-    -v $(pwd)/config.yaml:/etc/os-net-config/config.yaml \
-    -v /etc/sysconfig/network-scripts:/etc/sysconfig/network-scripts  \
-    -v /lib/modules:/lib/modules:ro \
+    --detach \
+    --volume $(pwd)/config.yaml:/etc/os-net-config/config.yaml \
+    --volume /etc/sysconfig/network-scripts:/etc/sysconfig/network-scripts  \
+    --volume /lib/modules:/lib/modules:ro \
     opstool/os-net-config \
     os-net-config -d
